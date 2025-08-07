1f6b190c7fefcb39866122c1eb871260f3125724
Atieh <atieh.saberi@gmail.com>
2025-08-07 14:39:17 -0400

    Add test directory

---
+++ src/main/scala/test/TestMS.scala
@@ -0,0 +1,25 @@
+package test
+
+/****class TestMS {
+
+  
+  def result(num: Int): List[Integer] = {
+    var result: List[Integer] = List()
+    
+    result:+= fibonacci(num)
+    result
+    
+  }
+  def fibonacci(num:Int): Integer = {
+    var result: List[Integer] = List()
+    while(fibonacci(num < num)
+    if(num == 0) result:+=0; 
+    if(num == 1) 0
+      if(num == 2) 1
+
+      fibonacci(num - 1) + result(num -2)
+  
+    
+  }
+}
+***/
